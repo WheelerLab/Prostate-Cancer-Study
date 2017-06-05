ea7db77a8f79fb908e566b3da98c37fbe238b83d
mabdulsami <mabdulsami@luc.edu>
2017-06-05 11:55:53 -0500

    Update Mo-GWAS QC.sh

--- Mo-GWAS QC.sh
+++ Mo-GWAS QC.sh
@@ -1,9 +1,13 @@
-Making a directory for the AA data, and then creating one file instead of multiple step files.
+#Making a directory for the AA data, and then creating one file instead of multiple step files.
 
 mkdir px_prostate_cancer_AA
 touch QC
 
 plink --bfile /home/mohammed/GENEVA_MEC_ProstateCancer_AA_all -missing --out /home/mohammed/px_prostate_cancer_AA/QC
+# bfile = pulls from the 3 binary files, bed, bim, fam
+# /home/mohammed/GENEVA_MEC_ProstateCancer_AA_all = This is where all the consolidated data is, where we are pulling from
+# -missing = Sample missing data report written to /home/mohammed/px_prostate_cancer_AA/QC.imiss, and variant-based missing data report written to /home/mohammed/px_prostate_cancer_AA/QC.lmiss
+# /home/mohammed/px_prostate_cancer_AA/QC = where data is being processed towards.
 PLINK v1.90b4.3 64-bit (9 May 2017)            www.cog-genomics.org/plink/1.9/
 (C) 2005-2017 Shaun Purcell, Christopher Chang   GNU General Public License v3
 Logging to /home/mohammed/px_prostate_cancer_AA/QC.log.
@@ -27,6 +31,8 @@ Total genotyping rate is 0.935254.
 --missing: Sample missing data report written to
 /home/mohammed/px_prostate_cancer_AA/QC.imiss, and variant-based missing data
 report written to /home/mohammed/px_prostate_cancer_AA/QC.lmiss.
+
+
 
     
      
