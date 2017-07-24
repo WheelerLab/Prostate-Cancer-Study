0aa6378d5f0a57864c24985523162b08a2f892b4
mabdulsami <mabdulsami@luc.edu>
2017-07-24 12:13:21 -0500

    Update and rename 5_GWASQC_LA to 5_GWASQC_LA.sh

---
+++ 5_GWASQC_LA.sh
@@ -0,0 +1,6 @@
+touch QC
+
+#We are starting off with 2018 people
+
+--bfile /home/mohammed/GENEVA_MEC_ProstateCancer_AA_all -missing --out /home/mohammed/px_prostate_cancer_AA/QC
+plink --bfile /home/mohammed/px_prostate_cancer_LA 
